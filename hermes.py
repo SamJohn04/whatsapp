cc1f2cee2fac06dad4acb793560ad505ef5de585
Samuel John <samjohnmalayam@gmail.com>
2022-09-14 08:07:38 +0530

    Oops... Looks like there were a tad few errors which I missed. Should've had my coffe back then.

--- hermes.py
+++ hermes.py
@@ -39,41 +39,43 @@ def readList():
     addG=Button(read,text="Add as Group.",command=asG)
     addG.grid(row=1,column=2)
     read.mainloop()
-    fn=open("../App/numbers.txt","a")
+    fn=open("numbers.txt","a")
     for n in reci:
+        fn.write('\n')
         fn.write(n)
     fn.close()
-    fg=open("../App/groups.txt","a")
+    fg=open("groups.txt","a")
     for g in recig:
+        fg.write('\n')
         fg.write(g)
     fg.close()
 def greetbytime():
     getList()
     for n in recilist:
         if t.tm_hour<=10:
-            sendwhatmsg_instantly(n, 'Good Morning!')
+            sendwhatmsg_instantly(n, 'Good Morning!',4,True,2)
         elif t.tm_hour<13:
-            sendwhatmsg_instantly(n,'Good Day!')
+            sendwhatmsg_instantly(n,'Good Day!',4,True,2)
         elif t.tm_hour<17:
-            sendwhatmsg_instantly(n,'Good Afternoon!')
+            sendwhatmsg_instantly(n,'Good Afternoon!',4,True,2)
         else:
-            sendwhatmsg_instantly(n,'Good Evening!')
+            sendwhatmsg_instantly(n,'Good Evening!',4,True,2)
     for g in reciglist:
         if t.tm_hour<=10:
-            sendwhatmsg_to_group_instantly(g, 'Good Morning!')
+            sendwhatmsg_to_group_instantly(g, 'Good Morning!',4,True,2)
         elif t.tm_hour<13:
-            sendwhatmsg_to_group_instantly(g,'Good Day!')
+            sendwhatmsg_to_group_instantly(g,'Good Day!',4,True,2)
         elif t.tm_hour<17:
-            sendwhatmsg_to_group_instantly(g,'Good Afternoon!')
+            sendwhatmsg_to_group_instantly(g,'Good Afternoon!',4,True,2)
         else:
-            sendwhatmsg_to_group_instantly(g,'Good Evening!')
+            sendwhatmsg_to_group_instantly(g,'Good Evening!',4,True,2)
 
 def justGreet():
     getList()
     for n in recilist:
-        sendwhatmsg_instantly(n,'Hello!!')
+        sendwhatmsg_instantly(n,'Hello!!',4,True,2)
     for g in reciglist:
-        sendwhatmsg_to_group_instantly(g, 'Hello!')
+        sendwhatmsg_to_group_instantly(g, 'Hello!',4,True,2)
 
 def somethingElse():
     getList()
@@ -83,9 +85,9 @@ def somethingElse():
     def proceed():
         x=e.get()
         for n in recilist:
-            sendwhatmsg_instantly(n,x)
+            sendwhatmsg_instantly(n,x,4,True,2)
         for g in reciglist:
-            sendwhatmsg_to_group_instantly(g, x)
+            sendwhatmsg_to_group_instantly(g, x,4,True,2)
         e.delete(0,END)
 
     oke=Button(read,text="Ok!",command=proceed)
